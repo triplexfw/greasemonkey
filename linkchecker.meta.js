529a44a6e946d7d69d8921711e58dc2667562a77
triplexfw <triplex@filewarez.tv>
2016-03-16 06:33:34 -0300

    linkchecker xor

---
+++ linkchecker.meta.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name        Verificador de uploads [FW]
+// @description Userscript para auxiliar na verificação de uploads na FileWarez.
+// @include     /^http:\/\/(www\.)?filewarez\.tv/.*$/
+// @copyright   2015, XOR
+// @author      Triplex,Portal_games
+// @version     0.3.1
+// @license     MIT License
+// @grant       unsafeWindow
+// @grant       GM_xmlhttpRequest
+// @require     https://www.googledrive.com/host/0B10BK7prllenfi1uWXR1NlZGdzBRV21GVnVJREVPTnJGUDRIXzh0dDk3ZmNsX09oOE9KUE0/asmcrypto.js
+// @updateURL   https://linkchecker.github.io/releases/linkchecker.meta.js
+// @downloadURL https://linkchecker.github.io/releases/linkchecker.user.js
+// @namespace   4971e63b909612f5598edd6d90012dae
+// @run-at      document-start
+// ==/UserScript==
