b8cf4ab3a55c4c6646ee84c2ed89d3e1c11abf92
triplexfw <triplex@filewarez.tv>
2017-04-22 23:08:20 -0300

    Update fwp2m.user.js

--- fwp2m.user.js
+++ fwp2m.user.js
@@ -2,7 +2,7 @@
 // @name        fwp2m
 // @namespace   filewarez
 // @description filewarez p2m
-// @include     https://filewarez.tv/*
+// @include     /^http(s)?:\/\/(www\.)?filewarez\.tv/.*$/
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
 // @updateURL   https://raw.githubusercontent.com/triplexfw/greasemonkey/master/fwp2m.meta.js
 // @downloadURL https://raw.githubusercontent.com/triplexfw/greasemonkey/master/fwp2m.user.js
