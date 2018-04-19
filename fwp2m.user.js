3d1c57aa6f79789069f6a705145b8d99db328051
triplexfw <triplex@filewarez.tv>
2018-04-19 15:18:47 -0300

    atualização
    fix bug

--- fwp2m.user.js
+++ fwp2m.user.js
@@ -4,10 +4,10 @@
 // @description filewarez p2m
 // @include     /^http(s)?:\/\/(www\.)?filewarez\.tv/.*$/
 // @include     /^http(s)?:\/\/(www\.)?filewarez\.ml/.*$/
-// @require     http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
+// @require     http://ajax5.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
 // @updateURL   https://raw.githubusercontent.com/triplexfw/greasemonkey/master/fwp2m.meta.js
 // @downloadURL https://raw.githubusercontent.com/triplexfw/greasemonkey/master/fwp2m.user.js
-// @version     1.22
+// @version     1.23
 // @grant       none
 // @run-at      document-start
 // ==/UserScript==
@@ -24,13 +24,9 @@ function iniciar() {
                janela.innerHTML = '<button type="button" onclick="location.hash=\'#upload_check_all\'">LINK</button>&nbsp;&nbsp;&nbsp;' +
                '<button type="button" onclick="location.hash=\'#postlist\'">TOPO</button>';
               janela.setAttribute('id', 'novo');
-            var elemento =  document.body.appendChild(janela); 
-  
-           var cf = '#postlist'
+            var elemento =  document.body.appendChild(janela);
+            var cf = '#postlist'
            $(cf).after(elemento); 
   }
  }
-setTimeout(iniciar, 2000);
-setTimeout(iniciar, 5000);
-setTimeout(iniciar, 10000);
-setTimeout(iniciar, 20000);
+document.onload = iniciar()
