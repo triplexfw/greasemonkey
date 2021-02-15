d11c27364a571dd1d231edfda196a5e070261b9f
triplexfw <triplex@filewarez.tv>
2021-02-15 13:00:03 -0300

    Update fwp2m.user.js
    melhorias.

--- fwp2m.user.js
+++ fwp2m.user.js
@@ -6,26 +6,50 @@
 // @include     /^http(s)?:\/\/(www\.)?filewarez\.ml/.*$/
 // @updateURL   https://github.com/triplexfw/greasemonkey/raw/master/fwp2m.meta.js
 // @downloadURL https://github.com/triplexfw/greasemonkey/raw/master/fwp2m.user.js
-// @version     1.25
+// @version     1.26
 // @grant       none
 // @run-at      document-idle
 // ==/UserScript==
 
-
+function copyToClipboard(element) {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    var element1 = localStorage.getItem('p2mcopy')
+    var element = element1 + element
+    localStorage.setItem('p2mcopy', element)
+    $temp.val(element).select();
+    document.execCommand("copy");
+    $temp.remove();
+}
+function copiap2m() { 
+  $('.upload_link').each((i, item) => {
+    if($('.link_status', item).hasClass('online')){
+        copyToClipboard($('.link_username', item).val());
+    }
+});
+                   }
 function iniciar() {
+ 
   var linkado = document.getElementById('upload_check_all')
  if (linkado) { 
   var janela = document.createElement('ul');
  
-               janela.style = 'top: 50%; left: 95%; position: fixed; margin: -30px 0 0 -50px; width: 100px; height: 15px; ' +
+               janela.style = 'top: 50%; left: 95%; position: fixed; margin: -30px 0 0 -80px; width: 140px; height: 35px; ' +
                  'padding: 20px; background: #fff; z-index: 999999999;';
               
                janela.innerHTML = '<button type="button" onclick="location.hash=\'#upload_check_all\'">LINK</button>&nbsp;&nbsp;&nbsp;' +
-               '<button type="button" onclick="location.hash=\'#postlist\'">TOPO</button>';
+               '<button type="button" onclick="location.hash=\'#postlist\'">TOPO</button><br>';
+               if ($('.link_username').val()) { janela.innerHTML = '<button type="button" onclick="location.hash=\'#upload_check_all\'">LINK</button>&nbsp;&nbsp;&nbsp;' +
+                 '<button type="button" onclick="location.hash=\'#postlist\'">TOPO</button><br>' +
+                 '<br><button type="button" id="copiap2mid">Copy P2M</button>&nbsp;<button type="button" id="limparcopiaid">limpar copia</button>'; }
               janela.setAttribute('id', 'novo');
             var elemento =  document.body.appendChild(janela);
             var cf = '#postlist'
            $(cf).after(elemento); 
+           $('#copiap2mid').click(copiap2m)
+           $('#limparcopiaid').click(function delcopy() { localStorage.setItem('p2mcopy', '')})
   }
  }
+
+
 setTimeout(iniciar, 2000);
