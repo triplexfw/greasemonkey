287edcaaffade9f78d9ab1ad551e290e1bb34225
triplexfw <triplex@filewarez.tv>
2022-04-09 23:16:37 -0300

    Update fwp2m.meta.js

--- fwp2m.meta.js
+++ fwp2m.meta.js
@@ -6,7 +6,7 @@
 // @include     /^http(s)?:\/\/(www\.)?filewarez\.ml/.*$/
 // @updateURL   https://github.com/triplexfw/greasemonkey/raw/master/fwp2m.meta.js
 // @downloadURL https://github.com/triplexfw/greasemonkey/raw/master/fwp2m.user.js
-// @version     1.26
+// @version     1.27
 // @grant       none
 // @run-at      document-start
 // ==/UserScript==
